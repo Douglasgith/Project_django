f7025e82247bcaf0724405d60feb99b75187effb
Douglas <dgsekofoz@gmail.com>
2024-02-07 14:09:02 -0300

    Imagnes e filtros / Mecanismo de busca

--- galeria/views.py
+++ galeria/views.py
@@ -5,9 +5,19 @@ from galeria.models import Fotografia
 
 def index(request):
 
-    fotografias = Fotografia.objects.order_by ("data_fotografia").filter(publicada=True)
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html',{"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia =  get_object_or_404(Fotografia, pk=foto_id)
     return render (request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        name_a_buscar = request.GET['buscar']
+        if name_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=name_a_buscar)
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
